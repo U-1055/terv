22d5e2e41c9b569d37d2f674e55be7605a1f0224
U-1055 <filatov_truba@mail.ru>
2025-12-18 22:38:57 +0700

    Реализован слой для работы с файлами клиента

---
+++ terv/src/base.py
@@ -0,0 +1,22 @@
+
+
+class DataStructConst:
+
+    access_token = 'access_token'
+    refresh_token = 'refresh_token'
+    userflow_widgets = 'userflow_widgets'
+    settings = 'settings'
+    style = 'style'
+
+    styles_paths = {
+        'style_1': r'..\data\resources\ui\styles\light_theme.qss',
+        'style_2': r'..\data\resources\ui\styles\dark_theme.qss'
+    }
+
+    names_widgets = {  # Типы виджетов и их имена
+
+    }
+
+    x, y = 'x', 'y'
+    x_size = 'x_size'
+    y_size = 'y_size'
