e87b688b251afec12bc6338c453b80ce2cf899a7
U-1055 <filatov_truba@mail.ru>
2026-01-12 18:58:16 +0700

    Переписан Requester

--- client/src/requester/requester.py
+++ client/src/requester/requester.py
@@ -1,8 +1,7 @@
 """Слой API."""
-import json
-
 import httpx
 
+import json
 import time
 import typing as tp
 import asyncio
@@ -39,97 +38,114 @@ class Requester:
         self._server = server
         self._struct = common_data_struct
 
+    def _prepare_response(self, response: 'Response'):
+        """Обрабатывает ответ: вызывает исключения, если запрос неудачный, передаёт ответ дальше, если успешный"""
+        error_code = response.error_id
+        message = response.message
+
+        if error_code != ErrorCodes.ok:  # Вызов исключения по коду
+            exc = err.exceptions_error_ids.get(error_code)
+            raise exc(message)
+
     async def _get(self, path: str, query_params: dict = None, headers: dict = None) -> 'Response':  # Базовые методы запросов
         async with httpx.AsyncClient() as client:
             result = await client.get(path, headers=headers, params=query_params)
-        return Response(result)
+
+        try:
+            return self._prepare_response(Response(result))
+        except err.APIError as e:
+            raise e
 
     async def _post(self, path: str, query_params: dict = None, headers: dict = None, json_: dict = None) -> 'Response':
         async with httpx.AsyncClient() as client:
-            result = await client.post(path, json=json, headers=headers, params=query_params)
-        return Response(result)
+            result = await client.post(path, json=json_, headers=headers, params=query_params)
+        try:
+            return self._prepare_response(Response(result))
+        except err.APIError as e:
+            raise e
 
     async def _delete(self, path: str, query_params: dict = None, headers: dict = None) -> 'Response':
         async with httpx.AsyncClient() as client:
             result = await client.delete(path, headers=headers, params=query_params)
-        return Response(result)  # ToDo: переписать запросы с новыми методами
+        try:
+            return self._prepare_response(Response(result))
+        except err.APIError as e:
+            raise e
 
     async def _put(self, path: str, query_params: dict = None, headers: dict = None, json_: dict = None) -> 'Response':
         async with httpx.AsyncClient() as client:
-            result = await client.put(path, json=json, headers=headers, params=query_params)
-        return Response(result)
+            result = await client.put(path, json=json_, headers=headers, params=query_params)
+        try:
+            return self._prepare_response(Response(result))
+        except err.APIError as e:
+            raise e
 
     @synchronized_request
     async def register(self, login: str, password: str, email: str):
-        async with httpx.AsyncClient() as client:
-            result = await client.post(f'{self._server}/register', json={self._struct.login: login, self._struct.password: password, self._struct.email: email})
-        error_code = result.json().get(self._struct.error_id)
-        message = result.json().get(self._struct.message)
-
-        if error_code != ErrorCodes.ok:  # Вызов исключения по коду
-            exc = err.exceptions_error_ids.get(error_code)
-            raise exc(message)
-
-        return result.json().get(self._struct.content)
+        try:
+            response = await self._post(
+                f'{self._server}/register',
+                json_={
+                    self._struct.login: login,
+                    self._struct.password: password,
+                    self._struct.email: email
+                }
+            )
+            return response.content.get(self._struct.content)
+        except err.APIError as e:
+            raise e
 
     @synchronized_request
     async def update_tokens(self, refresh_token: str) -> dict:
-        async with httpx.AsyncClient() as client:
-            result = await client.post(f'{self._server}/auth/refresh', json={self._struct.refresh_token: refresh_token})
-
-        error_code = result.json().get(self._struct.error_id)
-        message = result.json().get(self._struct.message)
-
-        if error_code != ErrorCodes.ok:  # Вызов исключения по коду
-            exc = err.exceptions_error_ids.get(error_code)
-            raise exc(message)
-
-        return result.json().get(self._struct.refresh_token)
+        try:
+            response = await self._post(
+                f'{self._server}/auth/refresh',
+                json_={self._struct.refresh_token: refresh_token}
+            )
+            return response.content.get(self._struct.refresh_token)
+        except err.APIError as e:
+            raise e
 
     @synchronized_request
     async def authorize(self, login: str, password: str) -> dict:
-        async with httpx.AsyncClient() as client:
-            result = await client.post(f'{self._server}/auth/login', json={self._struct.login: login, self._struct.password: password})
-
-        if result.status_code == 400:
-            raise err.UnknownCredentials
-
-        return result.json().get(self._struct.content)
-
-    @synchronized_request
-    async def get_user_info(self, access_token: str):
-        async with httpx.AsyncClient() as client:
-            result = await client.get(f'{self._server}/users', headers={'Authorization': access_token})
-
-        if result.status_code == 401:
-            raise err.ExpiredAccessToken
-        if result.status_code == 500:
-            raise err.ServerError
-
-        return result.json().get(self._struct.content)
+        try:
+            response = await self._post(
+                f'{self._server}/auth/login',
+                json_={self._struct.login: login, self._struct.password: password}
+            )
+            return response.content.get(self._struct.content)
+        except err.APIError as e:
+            raise e
 
     @synchronized_request
-    async def get_personal_tasks(self, user_id: int, access_token: str):
-        async with httpx.AsyncClient() as client:
-            result = await client.get(f'{self._server}/personal_tasks')
+    async def get_user_info(self, access_token: str, ):
+        try:
+            response = await self._get(f'{self._server}/users', headers={'Authorization': access_token})
+            return response.content.get(self._struct.content)
+        except err.APIError as e:
+            raise e
 
-        if result.status_code == 401:
-            raise err.ExpiredAccessToken
-        if result.status_code == 500:
-            raise err.ServerError
-
-        return result.json().get(self._struct.content)
+    @synchronized_request
+    async def get_personal_tasks(self, user_id: int, access_token: str, task_id: int = None):
+        """Получает личные задачи (конкретную или по user_id)."""
+        try:
+            path = f'{self._server}/personal_tasks'
+            if task_id:
+                path = f'{self._server}/personal_tasks/{task_id}'
+            response = await self._get(path, headers={'Authorization': access_token})
+            return response.content.get(self._struct.content)
+        except err.APIError as e:
+            raise e
 
 
 class Response:
     """
     Ответ API.
 
-    :var response: объект httpx.Response.
-    :var http_code: код статуса HTTP.
+    :var http_code: Код статуса HTTP.
     :var error_id: ID ошибки. Если JSON в ответе отсутствует, error_id = 500 Server Error.
-    :var content: содержимое ответа (только для успешных запросов). Если JSON отсутствует, content = None.
-    :var message: сообщение из ответа.
+    :var content: Содержимое ответа (только для успешных запросов). Если JSON отсутствует, content = None.
+    :var message: Сообщение из ответа.
     """
 
     def __init__(self, response: httpx.Response):
